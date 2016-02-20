5298317ba1184d591dbf78f332282f8b201cfd90
Jeremy Tubbs <jeremytubbs@gmail.com>
2016-02-20 11:43:47 -0500

    all asset types in sitemap

--- src/Http/Controllers/IgorSitemapController.php
+++ src/Http/Controllers/IgorSitemapController.php
@@ -25,27 +25,31 @@ class IgorSitemapController extends Controller
 
         // check if there is cached sitemap and build new only if is not
         if (!$sitemap->isCached()) {
-            // get all posts from db, with image relations
-            $posts = \App::make('\\App\\Post')
+            foreach (config('igor.type_routes') as $type => $route) {
+                $model = "App\\" . $type;
+                // get all posts from db, with image relations
+                 $posts = \App::make($model)
                     ->with('assets', 'assets.source', 'assets.type')
                     ->orderBy('created_at', 'desc')
                     ->get();
 
-            // add every post to the sitemap
-            foreach ($posts as $post) {
-                if($post->published == 1) {
-                    $images = [];
-                    foreach ($post->assets as $asset) {
-                        if ($asset->type->name == 'preview') {
-                            $images[] = [
-                                'url' => $asset->uri,
-                                'title' => $asset->source->title,
-                                'caption' => $asset->source->caption
-                            ];
-                        }
-                    }
 
-                    $sitemap->add(\URL::to('posts/'.$post->slug), $post->updated_at, '0.8', 'weekly', $images);
+                // add every post to the sitemap
+                foreach ($posts as $post) {
+                    if($post->published == 1) {
+                        $images = [];
+                        foreach ($post->assets as $asset) {
+                            if ($asset->type->name == 'preview') {
+                                $images[] = [
+                                    'url' => $asset->uri,
+                                    'title' => $asset->source->title,
+                                    'caption' => $asset->source->caption
+                                ];
+                            }
+                        }
+
+                        $sitemap->add(\URL::to($route.'/'.$post->slug), $post->updated_at, '0.8', 'weekly', $images);
+                    }
                 }
             }
         }
